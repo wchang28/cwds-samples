5614c2d9f7989a572fede824c8dc9e8209325fe4
Wen Chang <wchang@cerberuscapital.com>
2017-07-12 15:48:32 -0400

    uyjyrujyujyruj

--- ts/index.ts
+++ ts/index.ts
@@ -8,10 +8,6 @@ export function init(router: express.Router) {
         res.jsonp({msg: "Hi from samples!"});
     });
 
-    router.get("/long-ret", (req: express.Request, res: express.Response) => {
-        setTimeout(() => {res.jsonp({msg: "Hi from long return!"});}, 30000);
-    });
-
     router.get("/message", (req: express.Request, res: express.Response) => {
         res.jsonp({msg: "Howday from the sample"});
     });
@@ -42,7 +38,20 @@ export function init(router: express.Router) {
     router.get("/add-one-to-n", (req: express.Request, res: express.Response) => {
         let rqd = getRequestData(req);
         res.jsonp({result: addOneToN(rqd.Query["n"])});
-    })
+    });
 
+    let NFactorial = (N: number) : number => { // = 1 * 2 * 3 * ... * N
+        let product = 1;
+        for (let i = 1; i <= N; i++)
+            product *= i;
+        return product;
+    }
+
+    router.get("/n-factorial", (req: express.Request, res: express.Response) => {
+        let rqd = getRequestData(req);
+        res.jsonp({result: NFactorial(rqd.Query["n"])});
+    });
+
+    // create sub api branch called /smartystreet
     router.use("/smartystreet", smartystreetRouter);
 }
