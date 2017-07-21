4fad2f9fc743a2802d9c10a3c34e105267e500d9
Wen Chang <wchang@cerberuscapital.com>
2017-07-21 14:39:34 -0400

    added more database change

--- ts/db-access/index.ts
+++ ts/db-access/index.ts
@@ -7,7 +7,7 @@ let router = express.Router()
 export {router as Router};
 
 let db_options_1: expressMSSQL.Options = {
-    configSrc: (req: express.Request) => ({server: "AWS-PRD-SQL01", database: "TestDB", options: {trustedConnection: true}})
+    configSrc: (req: express.Request) => ({server: "quantsql01", database: "QMarket", options: {trustedConnection: true}})
     ,msnodesqlv8: true
     ,connectedPoolCallback: (req: express.Request, conn: expressMSSQL.ConnectionPool) => {getRequestData(req).set("db_connection", conn);}
 };
@@ -16,18 +16,21 @@ router.use("/db-1", expressMSSQL.get(db_options_1));
 
 router.get("/db-1/query", Endware((rqd: IRequestData) => {
     let conn = rqd.get<expressMSSQL.ConnectionPool>("db_connection");
-    return conn.request().query("SELECT * FROM [dbo].[ids]").then((result: expressMSSQL.IResult<any>) => result.recordset);
+    return conn.request().query("SELECT TOP 100 * FROM [dbo].[ZipCode2CBSA]").then((result: expressMSSQL.IResult<any>) => result.recordset);
 }));
 
 let db_options_2: expressMSSQL.Options = {
-    configSrc: (req: express.Request) => ({server: "AWS-PRD-SQL01", database: "QMarket", options: {trustedConnection: true}})
+    configSrc: (req: express.Request) => ({server: "trdsql01-dev", database: "QData", options: {trustedConnection: true}})
     ,msnodesqlv8: true
     ,connectedPoolCallback: (req: express.Request, conn: expressMSSQL.ConnectionPool) => {getRequestData(req).set("db_connection", conn);}
 };
 
 router.use("/db-2", expressMSSQL.get(db_options_2));
 
-router.get("/db-2/query", Endware((rqd: IRequestData) => {
+router.get("/db-2/execute", Endware((rqd: IRequestData) => {
     let conn = rqd.get<expressMSSQL.ConnectionPool>("db_connection");
-    return conn.request().query("SELECT * FROM [dbo].[BusinessDays]").then((result: expressMSSQL.IResult<any>) => result.recordset);
+    return conn.request()
+    .input("userId", "4037e1a4a149c454d91d8c4d29063061")
+    .execute("[dbo].[stp_NodeJSGridGetUserProfile]")
+    .then((result: expressMSSQL.IResult<any>) => result.recordset);
 }));
