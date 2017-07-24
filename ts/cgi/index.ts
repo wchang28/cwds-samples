ff6c56a307a981a9e71592f26973da758e13b239
wchang28 <wchang28@hotmail.com>
2017-07-24 13:39:33 -0400

    added node example

--- ts/cgi/index.ts
+++ ts/cgi/index.ts
@@ -1,9 +1,10 @@
 import * as express from "express";
 import * as core from 'express-serve-static-core';
 import {CGIEndware, IRequestData} from "crowdsourcing-api";
+import * as path from 'path';
 
 let router = express.Router()
 export {router as Router};
 
 router.get("/curl-example", CGIEndware("text/html", (rqd: IRequestData) => rqd.CGIChildProcessLauncher.exec("curl https://www.google.com")));
-//router.get("/curl-example", CGIEndware("text/html", (rqd: IRequestData) => rqd.CGIChildProcessLauncher.exec("curl https://www.yahoo.com")));
+router.get("/node-example", CGIEndware("application/json", (rqd: IRequestData) => rqd.CGIChildProcessLauncher.exec("node " + path.join(__dirname, "./node/test.js"))));
---
+++ ts/cgi/node/test.ts
@@ -0,0 +1,1 @@
+process.stdout.write(JSON.stringify({msg: "h from node cgi example :-)"}));
